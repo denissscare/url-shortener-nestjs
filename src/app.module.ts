485636a1995789b8f1896327769e68ee3e99245c
denissscare <denissscare@gmail.com>
2023-10-29 05:02:45 +0800

    init postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UrlModule } from './url/url.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Url } from './url/url.model';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [Url],
+      autoLoadModels: true,
+    }),
+    UrlModule,
   ],
   controllers: [],
   providers: [],
