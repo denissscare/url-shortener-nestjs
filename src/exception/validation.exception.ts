dc24962644308e2e89fd7a4faef3b4de1385e976
denissscare <denissscare@gmail.com>
2023-10-29 05:37:21 +0800

    create custom exception for validation pipes

---
+++ src/exception/validation.exception.ts
@@ -0,0 +1,9 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class ValidationException extends HttpException {
+  message;
+  constructor(res) {
+    super(res, HttpStatus.BAD_REQUEST);
+    this.message = res;
+  }
+}
