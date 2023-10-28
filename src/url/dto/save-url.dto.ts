980ca2afba25f3c15fa66d2b2ee5040aee70f6b9
denissscare <denissscare@gmail.com>
2023-10-29 05:51:00 +0800

    add validation for dto

--- src/url/dto/save-url.dto.ts
+++ src/url/dto/save-url.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
 export class SaveUrlDto {
   @ApiProperty({ example: 'https://exampleUrl.com', description: 'URL' })
+  @IsString({ message: 'URL must be a string' })
+  @IsNotEmpty({ message: 'URL cant be empty' })
+  @IsUrl(undefined, { message: 'URL is not valid.' })
   readonly url: string;
 
+  @IsString({ message: 'Alias must be a string' })
   @ApiProperty({ example: 'AliasForURL', description: 'Alias. Can be empty.' })
   readonly alias: string;
 }
